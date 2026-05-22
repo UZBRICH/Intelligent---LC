ef3a4a9276bc77432f91c24d5f395473d5e0b38a
Muhammad Aziz <richuzb13@gmail.com>
2026-05-22 18:21:22 +0500

    Merged repository

--- learning_center/settings.py
+++ learning_center/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vh&!*@4)1q%wn^ii-fccvsxs%7h!_7n#wjq*20fg@xdjv=2co%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost', 'intelligent-lc.onrender.com').split(',')
 
 
 # Application definition
